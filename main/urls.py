9e0c2861031a898558c4da7bdc6832d92548be90
Muhammad Jordan <jordan.aziz18@gmail.com>
2024-11-20 11:11:46 +0700

    benerin

--- main/urls.py
+++ main/urls.py
@@ -13,16 +13,17 @@ from main.views import delete_camera
 app_name = 'main'
 
 urlpatterns = [
+    # Main Page
     path('', show_main, name='show_main'),
-    path('xml/', show_xml, name='show_xml'),
-    path('json/', show_json, name='show_json'),
-    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
+    path('api/xml/', show_xml, name='show_xml'),
+    path('api/json/', show_json, name='show_json'),
+    path('api/xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('api/json/<str:id>/', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('edit-camera/<uuid:id>', edit_camera, name='edit_camera'),
+    path('edit-camera/<uuid:id>/', edit_camera, name='edit_camera'),
     path('create-camera-entry/', create_camera_entry, name='create_camera_entry'),
-    path('delete-camera/<uuid:id>', delete_camera, name='delete_camera'),
-    path('create-tokocamera-form-ajax/', create_tokocamera_form_ajax, name='create_tokocamera_form_ajax'),
+    path('delete-camera/<uuid:id>/', delete_camera, name='delete_camera'),
+    path('create-camera-ajax/', create_tokocamera_form_ajax, name='create_camera_ajax'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.core import serializers
 from main.models import tokocamera
 from main.forms import tokocameraform
@@ -9,7 +8,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 import datetime
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
@@ -20,16 +19,13 @@ from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
-    
-
+    camera_entries = tokocamera.objects.filter(user=request.user)
     context = {
-    
         'name': request.user.username,
-        'name': 'Muhammad Jordan ',
+        'last_login': request.COOKIES.get('last_login'),
         'class': 'PBP KKI',
-        #'last_login': request.COOKIES('last_login'),
+        'camera_entries': camera_entries,  # Include camera entries in context
     }
-
     return render(request, "main.html", context)
 
 def create_camera_entry(request):
@@ -96,14 +92,14 @@ def logout_user(request):
     return response
 
 def edit_camera(request, id):
-    # Get mood entry based on id
-    camera = tokocamera.objects.get(pk = id)
+    try:
+        camera = tokocamera.objects.get(pk=id)
+    except tokocamera.DoesNotExist:
+        raise Http404("Camera entry not found")
 
-    # Set mood entry as an instance of the form
     form = tokocameraform(request.POST or None, instance=camera)
 
     if form.is_valid() and request.method == "POST":
-        # Save form and return to home page
         form.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
@@ -111,11 +107,12 @@ def edit_camera(request, id):
     return render(request, "edit_camera.html", context)
 
 def delete_camera(request, id):
-    # Get mood based on id
-    camera = tokocamera.objects.get(pk = id)
-    # Delete mood
+    try:
+        camera = tokocamera.objects.get(pk=id)
+    except tokocamera.DoesNotExist:
+        raise Http404("Camera entry not found")
+
     camera.delete()
-    # Return to home page
     return HttpResponseRedirect(reverse('main:show_main'))
 
 @csrf_exempt
